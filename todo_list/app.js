c087a6537e532148f2d8cd3217b1c4c54eddfac3
elijah751 <elijahfx43@gmail.com>
2024-03-16 21:52:19 +0100

    save my to do in locaStorange

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,45 +1,56 @@
-const todoList = [ ];
+const todoList = [];
+
+
+window.addEventListener('load', () => {
+    const savedTodoList = localStorage.getItem('todoList');
+    if (savedTodoList) {
+        todoList.push(...JSON.parse(savedTodoList));
+        renderTodoList();
+    }
+});
 
 renderTodoList();
 
 function renderTodoList() {
     let todoListHtml = '';
     todoList.forEach((todo, i) => {
-         todo = todoList[i];
         const {name, dueDate} = todo;
 
         const html = `
             <div>${name}</div>
-            <div>  ${dueDate}</div>
-
+            <div>${dueDate}</div>
             <button class="js-delete delete">Delete</button>
         `;
         todoListHtml += html;
     })
-    
+
     document.querySelector('.js-todoList').innerHTML = todoListHtml;
     document.querySelectorAll('.js-delete')
         .forEach((deleteButton, i) => {
             deleteButton.addEventListener('click', () => {
                 todoList.splice(i, 1);
+                todoListToLocalStorage(); 
                 renderTodoList();
             })
         })
-       
-}
 
+}
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
 
     const inputDate = document.querySelector('.js-todo-dueDate');
-
     const dueDate = inputDate.value;
-    todoList.push(
-        {name, dueDate}
-    );
+
+    todoList.push({name, dueDate});
+    todoListToLocalStorage(); 
     inputElement.value = '';
     renderTodoList();
-   
+
+}
+
+
+function todoListToLocalStorage() {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
 }
